80a0915df5c13572372093354937cc14b87a8617
keiran beaton <keiran beaton>
2016-09-12 14:49:34 -0700

    getting back data, need to decide where to call it and add data to mongo

--- backend/lib/park-data.js
+++ backend/lib/park-data.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const Park = require('../model/park');
+
 let request = require('request');
 
 var options = { method: 'GET',
@@ -14,24 +16,34 @@ var options = { method: 'GET',
 module.exports = exports = request(options, function (error, response, data) {
   if (error) throw new Error(error);
   let dataArray = JSON.parse(data);
-  let basketballCount = 0;
-  let basketballHalfCount = 0;
-  let soccerCount = 0;
-  dataArray.forEach(function(item) {
-    if(item.feature_desc === 'Basketball (Full)'){
-      basketballCount += 1;
-    }
-    if(item.feature_desc === 'Basketball (Half)') {
-      basketballHalfCount += 1;
-    }
-    if(item.feature_desc === 'Soccer') {
-      soccerCount += 1;
+  let filteredData = dataArray.filter(function(item) {
+    if(item.feature_desc === 'Basketball (Full)' || item.feature_desc === 'Basketball (Half)' || item.feature_desc === 'Soccer' || item.feature_desc === 'Tennis Court (Outdoor)' || item.feature_desc === 'Baseball/Softball') {
+      return true;
     }
   });
-  console.log('Data Array 0:', dataArray[0]);
-  console.log('Data Array 1:', dataArray[1]);
-  console.log('Data Array 2:', dataArray[2]);
-  console.log('full basketball:', basketballCount);
-  console.log('half basketball:', basketballHalfCount);
-  console.log('soccer:', soccerCount);
+  let formattedData = [];
+  filteredData.forEach(function(park) {
+    let newPark = {};
+    newPark.location = {};
+    newPark.sports = [];
+    newPark.name = park.name;
+    newPark.hours = park.hours;
+    newPark.location.ypos = parseFloat(park.ypos);
+    newPark.location.xpos = parseFloat(park.xpos);
+    newPark.sports.push(park.feature_desc);
+    formattedData.push(newPark);
+  });
+  let sortedData = [];
+  sortedData = formattedData.sort(function(a, b) {
+    let nameA = a.name.toUpperCase();
+    let nameB = b.name.toUpperCase();
+    if(nameA < nameB) {
+      return -1;
+    }
+    if(nameA > nameB) {
+      return 1;
+    }
+    return 0;
+  });
+  console.log('sorted Data:', sortedData);
 });
