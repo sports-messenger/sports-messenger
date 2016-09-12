ae2f93591d26464666802d8660c6ec8a2dd86655
keiran beaton <keiran beaton>
2016-09-12 11:16:02 -0700

    auth-controller set up in angular

--- app/controllers/auth-controller.js
+++ app/controllers/auth-controller.js
@@ -1,1 +1,30 @@
 'use strict';
+
+module.exports = function(app) {
+  app.controller('AuthController', ['$http', '$location', '$window', '$log', function($http, $location, $window, $log) {
+    this.signup = function(user) {
+      $log.debug('$ctrl.signup');
+      $http.post(this.baseUrl + '/signup', user)
+      .then((res) => {
+        $http.defaults.headers.common['Authorization'] = 'Bearer ' + res.data.token;
+        $location.path('/home');
+      }, (err) => {
+        $log.error('error in $ctrl.signup', err);
+      });
+    };
+
+    this.signin = function(user) {
+      $http.get(this.baseUrl + '/signin', {
+        headers: {
+          'Authorization': 'Basic ' + $window.btoa(user.basic.email + ':' + user.basic.password)
+        }
+      })
+      .then((res) => {
+        $http.defaults.headers.common['Authorization'] = 'Bearer ' + res.data.token;
+        $location.path('/home');
+      }, (err) => {
+        $log.error('error in $ctrl.signin', err);
+      });
+    };
+  }]);
+};
