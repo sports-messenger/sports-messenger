3278cd8b724a2cc6dcb79bc855771d1c5d8b43ca
ercain03 <ercain3@gmail.com>
2016-09-13 15:30:30 -0700

    Finally got the google maps to work with angular

--- app/components/index.js
+++ app/components/index.js
@@ -4,6 +4,7 @@ module.exports = (app) => {
   require('./sign-up')(app);
   require('./sign-in')(app);
   require('./park')(app);
+  require('./map')(app);
   require('./comment')(app);
   require('./comment-form')(app);
 };
