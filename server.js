c6fe4ae86f2a66deee7103ffed80d467e98d086b
Mars <marshallmiller92@gmail.com>
2016-09-13 12:55:46 -0700

    debugging

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 'use strict';
 
-const cors = require('cors');
+// const cors = require('cors');
 const morgan = require('morgan');
 const express = require('express');
 const mongoose = require('mongoose');
 const Promise = require('bluebird');
-const createError = require('http-errors');
+// const createError = require('http-errors');
 const debug = require('debug')('home:server');
 
 const handleError = require('./backend/lib/error-handler');
@@ -20,16 +20,16 @@ process.env.APP_SECRET = 'secret';
 mongoose.Promise = Promise;
 mongoose.connect(mongoDbUri);
 
-app.use(cors());
+// app.use(cors());
 app.use(morgan('dev'));
 
 app.use('/api', authRouter);
 app.use('/api', commentRouter);
 
-app.all('*', function(req, res, next){
-  debug('Got error: 404');
-  next(createError(404, `ERROR: ${req.method} :: ${req.url} is not a route`));
-});
+// app.all('*', function(req, res, next){
+  // debug('Got error: 404');
+  // next(createError(404, `ERROR: ${req.method} :: ${req.url} is not a route`));
+// });
 
 app.use(handleError);
 
