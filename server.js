0ea2484da6c2d4b90ac3dad9778dd0e436bf1971
Mars <marshallmiller92@gmail.com>
2016-09-13 13:24:30 -0700

    readded cors

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 'use strict';
 
-// const cors = require('cors');
 const morgan = require('morgan');
 const express = require('express');
 const mongoose = require('mongoose');
 const Promise = require('bluebird');
 const createError = require('http-errors');
 const debug = require('debug')('home:server');
+const cors = require('cors');
 
 const handleError = require('./backend/lib/error-handler');
 const authRouter = require('./backend/routes/auth-router');
@@ -14,14 +14,14 @@ const commentRouter = require('./backend/routes/comment-router');
 
 const app = express();
 const port = process.env.PORT || 3000;
-const mongoDbUri = process.env.MONGODB_URI || 'mongodb://localhost:3000';
+const mongoDbUri = process.env.MONGODB_URI || 'mongodb://localhost/home';
 process.env.APP_SECRET = 'secret';
 
 mongoose.Promise = Promise;
 mongoose.connect(mongoDbUri);
 
-// app.use(cors());
 app.use(morgan('dev'));
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.json({msg: 'hello'});
