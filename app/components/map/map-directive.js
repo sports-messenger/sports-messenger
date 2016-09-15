79792b592d037f5154196b44ca7b73ad9b9785be
keiran beaton <keiran beaton>
2016-09-15 01:51:49 -0700

    map and form on same page, updating on changes

--- app/components/map/map-directive.js
+++ app/components/map/map-directive.js
@@ -1,91 +1,20 @@
 'use strict';
 
 module.exports = (app) => {
-  app.directive('googleMap', ['$rootScope', '$http', 'lazyLoadApi', 'parksMapCombine', function($rootScope, $http, lazyLoadApi, parksMapCombine) {
+  app.directive('googleMap', function() {
     return {
+      controller: 'MapController',
+      controllerAs: 'mapCtrl',
+      template: require('./map-template.html'),
       restrict: 'CA',
+      bindToController: true,
       scope: {
         mapId: '@id',
         lat: '@',
-        long: '@'
-      },
-      link: function(scope, element, attrs) {
-        var location = null;
-        var map = null;
-        var mapOptions = null;
-        var mapPoints = [];
-
-
-        if (angular.isDefined(scope.lat) && angular.isDefined(scope.long)) {
-          lazyLoadApi.then( initializeMap );
-        }
-
-        function initializeMap() {
-          var location = new google.maps.LatLng(scope.lat, scope.long);
-          var image = 'https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BB1oXOM.img';
-          var mapOptions = {
-            zoom: 12,
-            center: location,
-            icon: image
-          };
-
-
-          var map = new google.maps.Map(element[0], mapOptions);
-
-          $http.get(`${__API_URL__}/api` + '/parks', {
-            headers: {
-              'Content-Type': 'appliction/json',
-              'Accept': 'application/json'
-            }
-          }).then(function(res){
-            mapPoints = res.data;
-            mapPoints.forEach(function(park) {
-              new google.maps.Marker({
-                position: new google.maps.LatLng(park.location.ypos, park.location.xpos),
-                map: map,
-                icon: image,
-                title: park.name
-              });
-
-              var contentString = '<div id="content">'+
-              '<div id="siteNotice">'+
-              '<p id="firstHeading">Location: '+ park.name +'</p>'+
-              '<p id="sportsParagraphs">Sports: '+ park.sports +'<p>'+
-              '<p id="locationHours">Hours: '+ park.hours +'<p>'+
-              '</div>'+
-              '</div>';
-
-              var infowindow =  new google.maps.InfoWindow({
-                content: contentString
-              });
-
-              marker.addListener('mouseover', function() {
-                infowindow.open(map, marker);
-              });
-
-              marker.addListener('mouseout', function() {
-                infowindow.close();
-              });
-            });
-          });
-
-          var marker = new google.maps.Marker({
-            position: location,
-            map: map,
-            icon: image,
-          });
-        }
-        function setNewMap() {
-          let newLocation = parksMapCombine.getAddressPoint();
-          location = new google.maps.LatLng(newLocation.lat, newLocation.lng);
-          mapOptions = {zoom: 12, center: location};
-          map = new google.maps.Map([element[0], mapOptions]);
-          mapPoints = parksMapCombine.getArray();
-          mapPoints.forEach(function(park) {
-            new google.maps.Marker({position: new google.maps.LatLng(park.location.ypos, park.location.xpos), map: map});
-          });
-        }
+        long: '@',
+        baseUrl: '@',
+        config: '='
       }
     };
-  }]);
+  });
 };
--- app/controllers/auth-controller.js
+++ app/controllers/auth-controller.js
@@ -2,20 +2,20 @@
 
 module.exports = function(app) {
   app.controller('AuthController', ['$http', '$location', '$window', '$log', 'auth', function($http, $location, $window, $log, auth) {
-    this.signup = function(user) {
-
-      $log.debug('$ctrl.signup');
-
-      $http.post(this.baseUrl + '/signup', user)
-      .then((res) => {
-        auth.setToken(res.data.token);
-        $http.defaults.headers.common['Authorization'] = 'Bearer ' + res.data.token;
-        $location.path('/home');
-      }, (err) => {
-        $log.error('error in $ctrl.signup', err);
-        $location.path('/home');
-      });
-    };
+    // this.signup = function(user) {
+    //
+    //   $log.debug('$ctrl.signup');
+    //
+    //   $http.post(this.baseUrl + '/signup', user)
+    //   .then((res) => {
+    //     auth.setToken(res.data.token);
+    //     $http.defaults.headers.common['Authorization'] = 'Bearer ' + res.data.token;
+    //     $location.path('/home');
+    //   }, (err) => {
+    //     $log.error('error in $ctrl.signup', err);
+    //     $location.path('/home');
+    //   });
+    // };
 
     this.login = function(user) {
       $http.get(this.baseUrl + '/login', {
