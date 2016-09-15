93a6629271678bdd62b65fc04201e6f7879c5c4a
keiran beaton <keiran beaton>
2016-09-14 17:53:43 -0700

    syntax errors in map directive

--- app/components/map/map-directive.js
+++ app/components/map/map-directive.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = (app) => {
-  app.directive('googleMap', ['$rootScope', '$http', 'lazyLoadApi', function($rootScope, $http, lazyLoadApi, parkMapCombine) {
+  app.directive('googleMap', ['$rootScope', '$http', 'lazyLoadApi', 'parksMapCombine', function($rootScope, $http, lazyLoadApi, parksMapCombine) {
     return {
       restrict: 'CA',
       scope: {
@@ -22,7 +22,7 @@ module.exports = (app) => {
 
         function initializeMap() {
           var location = new google.maps.LatLng(scope.lat, scope.long);
-          var image = 'https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BB1oXOM.img'
+          var image = 'https://img-s-msn-com.akamaized.net/tenant/amp/entityid/BB1oXOM.img';
           var mapOptions = {
             zoom: 12,
             center: location,
@@ -76,11 +76,11 @@ module.exports = (app) => {
           });
         }
         function setNewMap() {
-          let newLocation = parkMapCombine.getAddressPoint();
+          let newLocation = parksMapCombine.getAddressPoint();
           location = new google.maps.LatLng(newLocation.lat, newLocation.lng);
           mapOptions = {zoom: 12, center: location};
           map = new google.maps.Map([element[0], mapOptions]);
-          mapPoints = parkMapCombine.getArray();
+          mapPoints = parksMapCombine.getArray();
           mapPoints.forEach(function(park) {
             new google.maps.Marker({position: new google.maps.LatLng(park.location.ypos, park.location.xpos), map: map});
           });
