f72df2854006887401ce44cda8dce12a6a79387b
keiran beaton <keiran beaton>
2016-09-16 11:51:19 -0700

    auth working on post comment and map working super chill

--- app/controllers/auth-controller.js
+++ app/controllers/auth-controller.js
@@ -9,7 +9,6 @@ module.exports = function(app) {
       $http.post(this.baseUrl + '/signup', user)
       .then((res) => {
         auth.setToken(res.data.token);
-        $http.defaults.headers.common['Authorization'] = 'Bearer ' + res.data.token;
         $location.path('/map');
       }, (err) => {
         $log.error('error in $ctrl.signup', err);
@@ -26,7 +25,7 @@ module.exports = function(app) {
       })
       .then((res) => {
         auth.setToken(res.data.token);
-        $http.defaults.headers.common['Authorization'] = 'Bearer ' + res.data.token;
+        $log.log($http.defaults.headers.common);
         $location.path('/map');
       }, (err) => {
         $log.error('error in $ctrl.login', err);
