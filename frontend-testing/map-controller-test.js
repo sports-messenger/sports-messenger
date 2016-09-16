dc6274c7d0caf4a7976dce919d884bd3b8c6d813
keiran beaton <keiran beaton>
2016-09-15 21:05:58 -0700

    cannot test map-controller, park-controller tests written

---
+++ frontend-testing/map-controller-test.js
@@ -0,0 +1,24 @@
+// 'use strict';
+//
+// describe('testing map controller', function() {
+//   beforeEach(() => {
+//     angular.mock.module('projectApp');
+//     angular.mock.inject(($controller, $httpBackend) => {
+//       this.mapCtrl = new $controller('MapController');
+//       this.$httpBackend = $httpBackend;
+//       this.mapCtrl.baseUrl = 'http://localhost:3000/api';
+//       this.mapCtrl.config = {headers: {
+//         'Accept':'application/json', 'Content-Type':'application/json'
+//       }};
+//       this.mapCtrl.getUser = null;
+//     });
+//   });
+//
+//   it('testing initializeMap to see if we get data back', () => {
+//     this.mapCtrl.mapPoints = [{name:'test name'}];
+//     this.$httpBackend.expectGET('http://localhost:3000/api/parks', this.mapCtrl.config)
+//     .respond(200, [{name:'test name'}]);
+//     this.mapCtrl.initializeMap();
+//     this.$httpBackend.flush();
+//   });
+// });
